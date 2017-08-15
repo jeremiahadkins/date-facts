f72a879e9cfb7a479c6e5b730c789f89e1f84891
Jeremiah Adkins <hi@jeremiahadkins.com>
2017-08-14 20:50:53 -0400

    fixed seconds into day

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const moment = require('moment');
 const chalk = require('chalk');
 
 let now = moment();
-let minuteNow = now.minutes();
+
+let hoursPassed = Number(now.format('k')) * 3600;
+let minutesPassed = Number(now.format('m')) * 60;
+let secondsPassed = Number(now.format('s'));
+
+let secondsInDay = (hoursPassed + minutesPassed + secondsPassed);
 
 console.log('It is', chalk.blue(now.format('MMMM Do YYYY, h:mm:ss a')));
 console.log('It is the', chalk.magenta(now.format('DDDo')), 'day of the year.');
-console.log('It is', chalk.cyan(minuteNow * 60), 'seconds into the day.');
+console.log('It is', chalk.cyan(secondsInDay), 'seconds into the day.');
 
 if (now.isDST()) {
   console.log('It', chalk.green('is'), 'during Daylight Savings time.');
